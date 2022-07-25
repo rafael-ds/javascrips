b675d73917fd04979738bfa0ee76b5917a3df220
rafael-ds <radisilvads@gmail.com>
2022-07-25 15:15:24 -0300

    Refatorando as variaveis valuer para valuerNumber e valuerOperator

--- calculadora/src/js/script.js
+++ calculadora/src/js/script.js
@@ -3,10 +3,23 @@ const operators = document.querySelectorAll('[operator]')
 
 
 numbers.forEach(e => {
-    e.addEventListener('click', function(){
-        let valuer = e.value
+    e.addEventListener('click', function () {
+        let valueNumber = e.value
 
         let display = document.getElementById('display').innerHTML
-        document.getElementById('display').innerHTML = display + valuer
+        document.getElementById('display').innerHTML = display + valueNumber
+    })
+})
+
+operators.forEach(e => {
+    e.addEventListener('click', function () {
+        let valuerOperator = e.value
+
+        let display = document.getElementById('display').innerHTML
+        document.getElementById('display').innerHTML = display + valuerOperator
+
+        if (valuerOperator === '+') {
+            console.log('somar')
+        }
     })
 })
