ca9e5864d70e8624c28087705cb7e78e819e5ba9
rafael-ds <radisilvads@gmail.com>
2022-08-23 10:30:56 -0300

    ...

--- calculadora/src/js/script.js
+++ calculadora/src/js/script.js
@@ -2,15 +2,47 @@ const btnNumbers = document.querySelectorAll('[Number]')
 const btnOperators = document.querySelectorAll('[Operator]')
 const btnResult = document.querySelector('[Result]')
 
+const display = document.getElementById('display')
 
-btnNumbers.forEach(number => number.addEventListener(
-    'click', function () {
-        console.log(number.value)
-    }
-))
+let temNumber
+let numberOne
 
-btnOperators.forEach(operator => operator.addEventListener(
-    'click', function () {
-        console.log(operator.value)
-    }
-))
+function clickNumber() {
+    btnNumbers.forEach(eventClick => eventClick.addEventListener(
+        'click', function () {
+
+            console.log(eventClick.textContent)
+            temNumber = parseInt(eventClick.textContent)
+
+        }
+    ))
+}
+clickNumber()
+
+function clickOperator() {
+    btnOperators.forEach(eventClick => eventClick.addEventListener(
+        'click', function () {
+            console.log(eventClick.textContent)
+
+            if (eventClick.textContent == '+') {
+                numberOne = temNumber
+                temNumber = 0
+            }
+
+        }
+    ))
+}
+clickOperator()
+
+function clickResult() {
+    btnResult.addEventListener(
+        'click', function () {
+            console.log(btnResult.textContent)
+
+            console.log(temNumber + numberOne)
+        }
+    )
+}
+clickResult()
+
+
