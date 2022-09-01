df7c1865ee8c318b342cb06a1d3a70744238b643
rafael-ds <radisilvads@gmail.com>
2022-09-01 10:36:09 -0300

    Resolvendo o bug na hora da soma que retornava o primeiro valor somado por ele mesmo

--- calculadora/src/js/script.js
+++ calculadora/src/js/script.js
@@ -3,17 +3,18 @@ const btnOperators = document.querySelectorAll('[Operator]')
 const btnResult = document.querySelector('[Result]')
 
 
-let temNumber
+let temNumber = 0
 let numberOne
 
 function clickNumber() {
     btnNumbers.forEach(eventClick => eventClick.addEventListener(
         'click', function () {
             temNumber = eventClick.textContent
-            
-            let display = document.getElementById('display').innerHTML
-            temNumber = document.getElementById('display').innerHTML = display + temNumber
+            console.log(`Click Number ${temNumber}`)
 
+            let display = document.getElementById('display').innerHTML
+    
+            document.getElementById('display').innerHTML = display + temNumber
         }
     ))
 }
@@ -25,22 +26,34 @@ function clickOperator() {
 
             if (eventClick.textContent == '+') {
                 numberOne = temNumber
-                document.getElementById('display').innerHTML = ''
+                temNumber = 0
+                console.log(`Click NumberOne ${numberOne}`)
+                console.log(`Click Number 2 = ${temNumber}`)
+
+                
+                let display = document.getElementById('display').innerHTML
+                document.getElementById('display').innerHTML = display + ' + '
+                
             }
-
+            
         }
-    ))
-}
-clickOperator()
+        ))
+    }
+    clickOperator()
 
-function clickResult() {
-    btnResult.addEventListener(
-        'click', function () {
-            console.log(btnResult.textContent)
+    
+    function clickResult() {
+        btnResult.addEventListener(
+            'click', function () {
+                console.log(btnResult.textContent)
+                
+                console.log(`Click Number 3 = ${temNumber}`)
+            
+                let result =  parseInt(numberOne) + parseInt(temNumber)
+                document.getElementById('display').innerHTML = result
 
-            let result =  parseInt(temNumber) + parseInt(numberOne)
-            document.getElementById('display').innerHTML = result
-        }
+                console.log(result)
+            }
     )
 }
 clickResult()
